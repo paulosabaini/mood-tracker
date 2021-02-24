30e027b0893eddc35c9907890bd480df3dc75c10
Paulo Sabaini <paulo@sabaini.org>
2021-02-24 15:50:58 -0300

    Add bottom navigation menu.

--- app/src/main/java/org/sabaini/moodtracker/MainActivity.kt
+++ app/src/main/java/org/sabaini/moodtracker/MainActivity.kt
@@ -2,11 +2,30 @@ package org.sabaini.moodtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_MoodTracker)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+    }
+
+    fun setupViews() {
+        // Finding the Navigation Controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Setting Navigation Controller with the BottomNavigationView
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(
+            navController
+        )
     }
 }
